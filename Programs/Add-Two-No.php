8a7837f8b3a2117364d6dc5244514d246ba6349b
upraity <dheerajupraity03@gmail.com>
2024-02-19 14:42:29 +0530

    add two number

---
+++ Programs/Add-Two-No.php
@@ -0,0 +1,15 @@
+<?PhP
+// php is case insensitive programming lang.
+//with three variable 
+    $a = 10;
+    $b = 5;
+    $c = $a + $b;
+    EcHo ($c);
+
+    //for next line
+    echo "\n" ;
+//with only two variable
+    $x = 50;
+    $y = 4;
+    echo $x + $y;
+?>

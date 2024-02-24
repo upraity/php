f7d9e4e47139b81b5af226ea5e74c1f23aff8d03
upraity <dheerajupraity03@gmail.com>
2024-02-24 21:19:09 +0530

    Dates

---
+++ Programs/Dates.php
@@ -0,0 +1,25 @@
+<?php
+    //dates in php
+    
+    echo date("d"). "\n";     //Print date of today : 24
+    echo date("j"). "\n";     //Print date of today : 24
+    echo date("l"). "\n";     //Print day of today  : Saturday
+    echo date("D"). "\n";     //Print day of today  : Sat
+    echo date("S"). "\n";     //Print th of today   : TH
+    echo date("F"). "\n";     //Print month of today: February
+    echo date("M"). "\n";     //Print month of today: Feb
+    echo date("Y"). "\n";     //Print year of today : 2024
+    echo date("h"). "\n";     //Print hour          : 08
+    echo date("g"). "\n";     //Print hour          : 8
+    echo date("i"). "\n";     //Print minute        : 12 
+    echo date("s"). "\n";     //Print second        : 25
+    echo date("a"). "\n";     //Print am/pm         : am/pm
+    echo date("A"). "\n";     //Print AM/PM         : AM/PM 
+    echo date("d"). "\n";     //Print date day year : 02
+    echo date("n"). "\n";     //Print month of today: 2  
+
+    echo date("h:i:s A") ."\n"; //Print time        : 4:45:20 PM
+    echo date("dS D M Y")."\n"; //Print date        : 24th Sat Feb 2024
+              
+    //for more details go here:- https://www.php.net/manual/en/datetime.format.php
+?>

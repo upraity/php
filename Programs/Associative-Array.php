5a8cf8b137be6e0b46d295215446d9cb10b676d5
upraity <dheerajupraity03@gmail.com>
2024-02-26 12:44:14 +0530

    Associative Array

---
+++ Programs/Associative-Array.php
@@ -0,0 +1,28 @@
+<?php
+    //Numeric Array or Index Array
+    $arr = array('ram','shiv','kumar','arjun');
+
+    echo $arr[0];
+    echo $arr[1];
+    //using array for loop
+    for($i = 0; $i < count($arr); $i++){
+        echo $arr[$i]. "\n";
+    }
+
+    //using array foreach loop
+    foreach ($arr as $naam){
+        echo $naam. "\n";
+    }
+
+    //you can see that both loop has same output and you can use/choose any loop 
+
+    //Associative Array
+    $first = array('aman' => 'abh', 'shubham' => 'shubh', 'khushi' => 'happy', 'tanishka' => 'tanu', 'mohit' => 'o', 7 => 'dj'); //Using this symbol => we associate a value to another value (string) 
+    echo $first['aman']. "\n";
+    echo $first[7];
+
+    //Associative array usinf foreach loop
+        foreach ($first as $key => $value) { //where $key is a first value in array
+            echo "\n Short name of $key is $value";
+        }
+?>
--- Programs/Hello-World.php
+++ Programs/Hello-World.php
@@ -4,7 +4,7 @@ echo "Hello World!";
 
 
 <!--
-For embedding/use PHP we have to use <?php?>
+For embedding/use PHP we have to use <?php ?>
 all the php code will goes within the <?php  /*code*/  ?>
 above is the Hello World Programm written in PHP
 on line 2 we use 'echo' statement and then the tect Heelo World! as Strings

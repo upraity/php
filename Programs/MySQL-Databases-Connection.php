29c85159a29057140389e1ccb7921f1ef269509f
upraity <dheerajupraity03@gmail.com>
2024-03-08 15:54:40 +0530

    MySQL-Databases-Connection

--- Programs/MySQL-Databases-Connection.php
+++ Programs/MySQL-Databases-Connection.php
@@ -10,9 +10,10 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $database = "form";
 
     //create connection
-    $conn = mysqli_connect($servername, $username, $password);
+    $conn = mysqli_connect($servername, $username, $password, $database);
     if(!$conn){
         die('Connection fail!!'.mysqli_connect_error());
     }

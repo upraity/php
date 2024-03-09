e72ee50ba1f42faa8578f0699ea7697d25fd7e92
upraity <dheerajupraity03@gmail.com>
2024-03-09 22:03:07 +0530

    files function

--- Programs/Files.php
+++ Programs/Files.php
@@ -12,7 +12,7 @@
     }
     $content = fread($file,filesize("yoursfile.txt"));
     echo $content;
-    fclose($file);
+    fclose($file);//close the open file
 
     //r  = reading the file only
     //r+ = reading and writing the file
@@ -22,4 +22,19 @@
     //a+ = for reading and writing the file if the file does not exits then thay attempt to create the file
 
     //and more modes are available that you can see here: https://php.net/manual/en/function.fopen.php
+
+    //fgets() function
+
+    $file1 = fopen('yoursfile.txt','r');
+    echo fgets($file1); // only read the first line og the file
+    echo fgets($file1); // but when we write in new line fgets so they print second line 
+    
+    //fgetc() function
+    echo fgetc($file1); // read the file character by character
+
+    //if you use fgets() function but use only onces time so use this in inside the loop
+    while($a = fgets($file1)){
+        echo $a;
+    }
+
 ?>

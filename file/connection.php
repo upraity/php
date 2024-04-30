c4a7010ce0ab141d995208f8363e6410a8cd8948
Dj <134566303+upraity@users.noreply.github.com>
2024-04-30 22:07:50 +0530

    Create connection

---
+++ file/connection.php
@@ -0,0 +1,18 @@
+<!-- Q12. Wap to established a connects with the database on the server. -->
+<?php
+    // Database connection parameters
+    $servername = "localhost"; 
+    $username = "root";
+    $password = "";
+    $database = "discuss"; 
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    echo "Connected successfully";
+?>

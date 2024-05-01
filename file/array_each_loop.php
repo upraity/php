191700853d60a6699d7e77fb00040dd4220fc3aa
Dj <134566303+upraity@users.noreply.github.com>
2024-05-01 23:38:52 +0530

    Array using each looop

---
+++ file/array_each_loop.php
@@ -0,0 +1,9 @@
+<!-- Q16. Wap to print elements of an array using each loop.-->
+<?php
+    $colors = array("red", "green", "blue", "yellow");
+    
+    echo "<br>Using foreach loop:<br>";
+    foreach ($colors as $value) {
+        echo "Value: $value<br>";
+    }
+?>
